979558d919557e13be22034626825374e45658b8
Collin Wheat <collinwheat@gmail.com>
2022-12-19 16:56:51 -0500

    Objective3Lab1

---
+++ Labs/Objective3Lab1.java
@@ -0,0 +1,9 @@
+public class Objective3Lab1 {
+	public static void main(String[] args) {
+		int num1 = 10;
+		int num2 = 25;
+		int num3 = 31;
+		int average = (num1 + num2 + num3)/3;
+		System.out.println(average);
+	}
+}

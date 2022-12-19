a2fe7ed8f7a654ba1d2dc1e3d38483d12d14169b
Collin Wheat <collinwheat@gmail.com>
2022-12-19 14:54:30 -0500

    Objective2Lab3

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,10 @@
+public class Objective2Lab3 {
+	public static void main(String[] args) {
+		int Year = 1999;
+		String Month = "August";
+		Boolean Weekend = false;
+		System.out.println(Year);
+		System.out.println(Month);
+		System.out.println(Weekend);
+	}
+}

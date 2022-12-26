f3b13aa6699258c126cb6d04a9acb9bd4da788f8
Collin Wheat <collinwheat@gmail.com>
2022-12-26 13:38:09 -0500

    Objective7Lab4

---
+++ Labs/Objective7Lab4.java
@@ -0,0 +1,12 @@
+public class Objective7Lab4{
+	public static void main(String[] args){
+
+		int sum = 0;
+		int counter = 0;
+		while (counter < 20) {
+        counter ++;
+        sum = counter + sum;
+        System.out.println(sum);
+        }
+	}
+}
